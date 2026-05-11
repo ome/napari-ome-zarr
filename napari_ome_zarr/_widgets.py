14d007d18245a2d6b32a8d6cb954e8b909657767
Johannes Soltwedel <38459088+jo-mueller@users.noreply.github.com>
2026-05-11 13:07:59 +0200

    feat: add URL IO utility widget

---
+++ napari_ome_zarr/_widgets.py
@@ -0,0 +1,34 @@
+from __future__ import annotations
+
+import magicgui
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    import napari.types
+
+
+@magicgui.magic_factory(
+    call_button="Import",
+    ome_zarr_url={"label": "OME-ZARR URL"},
+)
+def import_from_url(ome_zarr_url: str) -> None:
+    """Import an OME-Zarr from a URL."""
+    from napari_ome_zarr._reader import napari_get_reader
+    from napari import current_viewer
+    viewer = current_viewer()
+
+    if viewer is None:
+        return
+
+    reader = napari_get_reader(ome_zarr_url)
+    if reader is None:
+        return None
+
+    layer_data = reader(ome_zarr_url)
+    for layer in layer_data:
+        if layer[2] == "image":
+            viewer.add_image(layer[0], **layer[1])
+        elif layer[2] == "labels":
+            viewer.add_labels(layer[0], **layer[1])
+        else:
+            pass
