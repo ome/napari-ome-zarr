60beb41152cb46c88485ec7c48c6d47cab124255
Juan Nunez-Iglesias <jni@fastmail.com>
2026-07-03 16:40:57 +0200

    Add an eager reader that loads data into RAM

--- napari_ome_zarr/_reader.py
+++ napari_ome_zarr/_reader.py
@@ -6,6 +6,7 @@ It implements the ``napari_get_reader`` hook specification, (to create a reader 
 import warnings
 from typing import Callable
 
+import numpy as np
 import zarr
 
 from .ome_zarr_reader import read_ome_zarr
@@ -31,3 +32,19 @@ def napari_get_reader(path: str | list) -> Callable | None:
     if group is not None:
         return read_ome_zarr(group)
     return None
+
+
+def napari_get_eager_reader(path: str | list) -> Callable | None:
+    read_ome_zarr_lazy = napari_get_reader(path)
+    if read_ome_zarr_lazy is not None:
+        def read_ome_zarr_eager(*args, **kwargs):
+            lazy_result = read_ome_zarr_lazy(*args, **kwargs)
+            eager_result = []
+            for data, metadata, layer_type in lazy_result:
+                if type(data) == list:  # multiscales
+                    eager_data = list(map(np.asarray, data))
+                else:  # single scale
+                    eager_data = np.asarray(data)
+                eager_result.append((eager_data, metadata, layer_type))
+            return eager_result
+        return read_ome_zarr_eager
